8f5d068eea154adf313ba7f0cb162d6e7a151eb2
Hunter John Larco <hjlarco@gmail.com>
2015-02-27 15:42:12 -0500

    Switch from Tabs to Spaces

--- build/overload.js
+++ build/overload.js
@@ -9,40 +9,40 @@
 
 (function(){
 	
-	var Overload = new Object();
-	Overload.function = function(){
-		var funct = function(){return funct['__overload__'].apply(funct, arguments)};
-		funct.overload = AddMethod.bind(funct);
-		return funct;
-	};
+  var Overload = new Object();
+  Overload.function = function(){
+    var funct = function(){return funct['__overload__'].apply(funct, arguments)};
+    funct.overload = AddMethod.bind(funct);
+    return funct;
+  };
 	
-	function AddMethod(funct, types){
-		if(GetType(types) != 'array') types = [];
+  function AddMethod(funct, types){
+    if(GetType(types) != 'array') types = [];
 		
-		for(var i=types.length; i<funct.length; i++) types.push('*');
-		var old = this['__overload__'];
+    for(var i=types.length; i<funct.length; i++) types.push('*');
+    var old = this['__overload__'];
 		
-		this['__overload__'] = function(){
-			if (funct.length == arguments.length && MatchesTypes(arguments, types)) return funct.apply(this, arguments);
-			else if (typeof old == 'function') return old.apply(this, arguments);
-		};
-	}
+    this['__overload__'] = function(){
+      if (funct.length == arguments.length && MatchesTypes(arguments, types)) return funct.apply(this, arguments);
+      else if (typeof old == 'function') return old.apply(this, arguments);
+    };
+  }
 	
-	function MatchesTypes(arguments, types){
-		for(var i=0; i<arguments.length; i++){
-			var arg = arguments[i],
-					type = types[i];
-			if(type == '*') continue;
-			if(GetType(arg) != type) return false;
-		}
-		return true;
-	}
-	function GetType(variable){
-		if(typeof variable != 'object') return typeof variable;
-		else if(variable.constructor == Array) return 'array';
-		else return 'object';
-	}
+  function MatchesTypes(arguments, types){
+    for(var i=0; i<arguments.length; i++){
+      var arg = arguments[i],
+          type = types[i];
+      if(type == '*') continue;
+      if(GetType(arg) != type) return false;
+    }
+    return true;
+  }
+  function GetType(variable){
+    if(typeof variable != 'object') return typeof variable;
+    else if(variable.constructor == Array) return 'array';
+    else return 'object';
+  }
 	
-	window.Overload = Overload;
+  window.Overload = Overload;
 	
 })();
